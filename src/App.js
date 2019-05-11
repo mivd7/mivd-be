541d99149fbdb631bd1a0ce7c57e112480c59bc2
MIVD <mi.vandeurzen@gmail.com>
2019-05-11 23:13:51 +0200

    utilize svg's

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Carousel from './components/layout/Carousel';
 import Item from './components/layout/Item';
 import Header from './components/Header';
 import musicalNote from './styles/svg/musical-note.svg';
-import next from './styles/svg/next-1.svg';
-import back from './styles/svg/back.svg';
+import macbook from './styles/svg/macbook2.svg';
+import glasses from './styles/svg/glasses.svg';
+import avatar from './styles/svg/avatar.svg';
 
 import './App.css'
 
@@ -19,16 +20,25 @@ export default class App extends Component {
           <p className="main-text-box">
             Swipe and click on a symbol to find out more about me
           </p>
-          <Carousel
-            title=""
-          >
+          <Carousel title="" >
             <Item>
-              <img src={musicalNote} className={"item-logo"}/>
+              <img src={musicalNote} className={"item-logo"} alt="music"/>
+              <label>Music</label>
             </Item>
 
-            <Item>Item 2</Item>
-            <Item>Item 3</Item>
-            <Item>Item 4</Item>
+            <Item>
+              <img src={macbook} className={"item-logo"} alt="developer"/>
+              <label>Coding</label>
+            </Item>
+
+            <Item>
+              <img src={glasses} className={"item-logo"} alt="historian"/>
+              <label>Education</label>
+            </Item>
+
+            <Item>
+              <img src={avatar} className={"item-logo"} alt="bio"/>
+              <label>Bio</label></Item>
           </Carousel>
         </div>
       </div>
