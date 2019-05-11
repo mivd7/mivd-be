d42d552135053d7f1eb82abd9d12f619ec096d47
MIVD <mi.vandeurzen@gmail.com>
2019-05-12 01:00:22 +0200

    image gallery

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import musicalNote from './styles/svg/musical-note.svg';
 import macbook from './styles/svg/macbook2.svg';
 import glasses from './styles/svg/glasses.svg';
+import growth from './styles/svg/growth.svg';
 import avatar from './styles/svg/avatar.svg';
 
 import './App.css'
@@ -20,26 +21,35 @@ export default class App extends Component {
           <p className="main-text-box">
             Swipe and click on a symbol to find out more about me
           </p>
+          <div className="item-gallery">
           <Carousel title="" >
-            <Item>
-              <img src={musicalNote} className={"item-logo"} alt="music"/>
-              <label>Music</label>
-            </Item>
+            
+              <Item>
+                <img src={musicalNote} className={"item-gallery__image"} alt="music"/>
+                <label>MIVD makes music</label>
+              </Item>
 
-            <Item>
-              <img src={macbook} className={"item-logo"} alt="developer"/>
-              <label>Coding</label>
-            </Item>
+              <Item>
+                <img src={macbook} className={"item-gallery__image"} alt="developer"/>
+                <label>MIVD makes web apps</label>
+              </Item>
 
-            <Item>
-              <img src={glasses} className={"item-logo"} alt="historian"/>
-              <label>Education</label>
-            </Item>
+              <Item>
+                <img src={glasses} className={"item-gallery__image"} alt="education"/>
+                <label>MIVD makes science</label>
+              </Item>
 
-            <Item>
-              <img src={avatar} className={"item-logo"} alt="bio"/>
-              <label>Bio</label></Item>
+              <Item>
+                <img src={growth} className={"item-gallery__image"} alt="skills"/>
+                <label>MIVD makes skills</label>
+              </Item>
+
+              <Item>
+                <img src={avatar} className={"item-gallery__image"} alt="bio"/>
+                <label>Short Bio</label>
+              </Item>
           </Carousel>
+          </div>
         </div>
       </div>
     );
